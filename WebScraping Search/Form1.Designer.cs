9eff4bc786196bf90bcdc99f9a58821f46700a87
Aries Penetrante <penetrantearies@gmail.com>
2021-01-25 05:26:21 +0200

    Add project files.

---
+++ WebScraping Search/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace WebScraping_Search
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.scrapeBtn = new System.Windows.Forms.Button();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(31, 25);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(415, 349);
+            this.outputTextBox.TabIndex = 1;
+            // 
+            // scrapeBtn
+            // 
+            this.scrapeBtn.Location = new System.Drawing.Point(31, 392);
+            this.scrapeBtn.Name = "scrapeBtn";
+            this.scrapeBtn.Size = new System.Drawing.Size(75, 23);
+            this.scrapeBtn.TabIndex = 2;
+            this.scrapeBtn.Text = "Scrape";
+            this.scrapeBtn.UseVisualStyleBackColor = true;
+            this.scrapeBtn.Click += new System.EventHandler(this.scrapeBtn_Click);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(483, 25);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(389, 404);
+            this.webBrowser.TabIndex = 3;
+            this.webBrowser.Url = new System.Uri("http://quotes.toscrape.com/login", System.UriKind.Absolute);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(140, 392);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 4;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(267, 392);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(75, 23);
+            this.exitBtn.TabIndex = 5;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 450);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.scrapeBtn);
+            this.Controls.Add(this.outputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Button scrapeBtn;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button exitBtn;
+    }
+}
+
---
+++ WebScraping Search/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+using System.Globalization;
+using System.Net.Http;
+using System.Threading;
+using Timer = System.Threading.Timer;
+
+
+namespace WebScraping_Search
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+        private void scrape()
+
+        {
+
+            string browserContents = webBrowser.DocumentText;
+
+            outputTextBox.Text = browserContents;
+        
+
+           // MatchCollection authorM = Regex.Matches(browserContents, @"author\s*(.+?)\s", RegexOptions.Singleline);
+
+           // outputTextBox.Text = Convert.ToString(authorM);
+           
+
+
+        }
+
+
+
+
+
+
+            private void scrapeBtn_Click(object sender, EventArgs e)
+        {
+
+            scrape();
+
+
+
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            outputTextBox.Text = string.Empty;
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.ExitThread();
+        }
+    }
+}
